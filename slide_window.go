2d2e021450fc103282f940ad753741e59e6ba513
elimsc <zhjiang.rtc@gmail.com>
2021-09-05 14:15:29 +0800

    update

--- slide_window.go
+++ slide_window.go
@@ -1,33 +1,27 @@
+package main
+
+// https://programmercarl.com/0209.%E9%95%BF%E5%BA%A6%E6%9C%80%E5%B0%8F%E7%9A%84%E5%AD%90%E6%95%B0%E7%BB%84.html#_209-%E9%95%BF%E5%BA%A6%E6%9C%80%E5%B0%8F%E7%9A%84%E5%AD%90%E6%95%B0%E7%BB%84
 func minSubArrayLen(target int, nums []int) int {
-	var windowSum int
-	var ans = len(nums) + 1
-	var left, right int
-
-	for right < len(nums) {
-		for windowSum < target && right < len(nums) { // 扩张window
-			windowSum += nums[right] // window为 [left, right]
-			if windowSum >= target {
-				windowLen := right - left + 1
-				if ans > windowLen {
-					ans = windowLen
-				}
-			}
-			right++ // window为 [left, right)
-		}
-		for windowSum >= target && left < right { // 收缩窗口
-			windowSum -= nums[left]
-			left++
-			if windowSum >= target {
-				windowLen := right - left
-				if ans > windowLen {
-					ans = windowLen
-				}
-			}
-		}
-	}
-
-	if ans == len(nums)+1 {
+	if len(nums) == 0 {
 		return 0
 	}
-	return ans
+	sum := 0
+	minLen := len(nums) + 1
+
+	l, r := 0, 0 // [l,r]
+	for ; r < len(nums); r++ {
+		sum += nums[r]
+		for sum >= target { // 收缩窗口
+			curLen := r - l + 1
+			if curLen < minLen {
+				minLen = curLen
+			}
+			sum -= nums[l]
+			l++
+		}
+	}
+	if minLen == len(nums)+1 {
+		return 0
+	}
+	return minLen
 }
